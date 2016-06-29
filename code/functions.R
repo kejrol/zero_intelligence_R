f7fe71f8fcbc0029fbb0492b5a6b91eaa725a193
kejrol <ciulkinkarol@gmail.com>
2016-06-29 22:07:05 +0200

    final version 1.0 ;)

--- code/functions.R
+++ code/functions.R
@@ -1,28 +1,10 @@
-# library(data.table)
-library(reshape2)
 
-
-# install packages if necessary and load all of them ----------------------
-
-installAndLoadPackages <- function(packageList)
-{
-  for (i in packageList) {
-    if (!(i %in% installed.packages()[,1])) { # check if package was installed
-      print(paste0('Installing package ', i, '...'))
-      install.packages(i)
-    } else print(paste0('Package ', i, ' already installed.'))
-  }
-  lapply(packageList, require, character.only = TRUE)
-}
-
-
-# run the simulation ------------------------------------------------------
 
 runSimulation <- function(conditions = "ticks < maxNumberOfTrades",
                           report = c("ticks", "efficiency",
                                      "transactionPrice", "actualSurplus"))
 {
-  NLDoCommand(1, "setup")
+  NLCommand("setup")
   NLDoCommandWhile(condition = conditions,
                    command = "go")
   
@@ -32,51 +14,36 @@ runSimulation <- function(conditions = "ticks < maxNumberOfTrades",
   return(temp)
 }
 
+runSimulationConstReport <- function(conditions = "ticks < maxNumberOfTrades",
+                                     report = c("ticks", "efficiency",
+                                                "transactionPrice", "actualSurplus"))
+{
+  NLCommand("setup")
+  temp <- NLDoReportWhile(condition = conditions,
+                          command = "go",
+                          reporter = report,
+                          as.data.frame = T,
+                          df.col.names = report,
+                          max.minutes = 10,
+                          nl.obj = NULL)
+  return(temp)
+}
 
-# set the parameters of the simulation -----------------------------------
-
-parametrizeAndRun <- function(constrained = T, nOfBuyers = 70, nOfSellers = 60,
-                        maxBuyVal = 170, maxSellCost = 180,
+parametrize <- function(constrained = T, nBuyers = 70, nSellers = 60,
+                        maxBVal = 170, maxSCost = 180,
                         conditions = "ticks < maxNumberOfTrades",
                         report = c("ticks", "efficiency",
-                                   "transactionPrice", "actualSurplus"))
+                                   "transactionPrice", "actualSurplus"),
+                        constReport = F)
 {
   if (constrained) {constr <- "true"} else {constr <- "false"}
   NLCommand(paste0("set constrained ", constr))
   
-  count <- 0
-  for (nBuyers in nOfBuyers)
-  {
-    # nBuyers <- 133
-    NLCommand(paste0("set numberOfBuyers ", nBuyers))
-    
-    for (nSellers in nOfSellers)
-    {
-      # nSellers <- 143
-      NLCommand(paste0("set numberOfSellers ", nSellers))
-      
-      for (maxBVal in maxBuyVal)
-      {
-        # maxBVal <- 101
-        NLCommand(paste0("set maxBuyerValue ", maxBVal))
-        
-        for (maxSCost in maxSellCost)
-        {
-          # maxSCost <- 30
-          NLCommand(paste0("set maxSellerCost ", maxSCost))
-          nBuyersV <- c(nBuyersV, nBuyers)
-          nSellersV <- c(nSellersV, nSellers)
-          maxSCostV <- c(maxSCostV, maxSCost)
-          maxBValV <- c(maxBValV, maxBVal)
-          
-          count <- count + 1
-          print(paste("Iteracja:", count, "| Parametry:", nBuyers, nSellers, maxBVal, maxSCost))
-          
-          df <- rbind(df, runSimulation(conditions,
-                                        report))
-        }
-      }
-    }
-  }
-  return(df)
+  NLCommand(paste0("set numberOfBuyers ", nBuyers))
+  
+  NLCommand(paste0("set numberOfSellers ", nSellers))
+  
+  NLCommand(paste0("set maxBuyerValue ", maxBVal))
+  
+  NLCommand(paste0("set maxSellerCost ", maxSCost))
 }
--- code/setup.R
+++ code/setup.R
@@ -1,57 +1,138 @@
 
 rm(list = ls())
 # .rs.restartR()
-
+options(strinsAsFactors = F)
 
 source("code/functions.R")
-installAndLoadPackages(c("data.table", "RNetLogo", "ggplot2"))
+library(reshape2)
+library(data.table)
+library(RNetLogo)
 library(ggplot2)
-# paths to NetLogo stuff --------------------------------------------------
+library(rmarkdown)
+library(gridExtra)
+
+ilePowtorzen <- 5 # ile powtorzen dla tych samych parametrow, w miejscach, w ktorych brana jest mediana realizacji
+zakresHeatmap <- c(35, 155) # wartosci pomiedzy ktorymi jest wykonywana symulacja do heatmapy
 
 path <- "/home/kejrol/netlogo/app"
 modelPath <- "models/Downloaded/ZITrading/ZITrading.nlogo"
 
-
-# starting the NetLogo session --------------------------------------------
-
-getwd()
-NLStart(nl.path = path) #, gui = F) # watch out, this changes the woking directory path
-getwd()
+oldPath <- getwd()
 # .rs.restartR()
+NLStart(nl.path = path, gui = F) # uwaga, ta funkcja zmiennia katalog roboczy
+getwd()
 NLLoadModel(model.path = modelPath)
+setwd(oldPath)
+# dane do wykresu przykladowego, 
+parametrize()
+df <- runSimulationConstReport()
+
+df <- melt(df, id.vars = "ticks", measure.vars = c("efficiency", "transactionPrice"),
+           factorsAsStrings = F)
+
+# wykres liniowy efektywnosci
+parametrize()
+ogr <- runSimulationConstReport(report = c("ticks", "efficiency"))
+ogr$racjonalnosc <- "agenci o ograniczonej\nracjonalności"
+parametrize(constrained = F)
+bOgr <- runSimulationConstReport(report = c("ticks", "efficiency"))
+bOgr$racjonalnosc <- "agenci nieracjonalni"
+
+ogry <- rbind(ogr, bOgr); rm(ogr, bOgr)
+
+# heatmapa pierwsza
+przejscie <- data.frame()
+for (i in seq(zakresHeatmap[1], zakresHeatmap[2], by = 10))
+{
+  # i <- 1
+  for (j in seq(zakresHeatmap[1], zakresHeatmap[2], by = 10))
+  {
+    parametrize(nBuyers = i, nSellers = j)
+    przejscie <- rbind(przejscie,
+                       data.frame(nOfBuyers = i,
+                                  nOfSellers = j,
+                                  efficiency = runSimulation(report = "efficiency")))
+    print(paste(i, j))
+  }
+}
+
+# heatmapa druga
+przejscie2 <- data.table()
+for (i in seq(przejscie[which.min(przejscie$efficiency),]$nOfBuyers - 5,
+              przejscie[which.min(przejscie$efficiency),]$nOfBuyers + 5,
+              by = 1))
+{
+  # i <- 1
+  for (j in seq(przejscie[which.min(przejscie$efficiency),]$nOfSellers - 5,
+                przejscie[which.min(przejscie$efficiency),]$nOfSellers + 5,
+                by = 1))
+  {
+    parametrize(nBuyers = i, nSellers = j)
+    for (a in 1:ilePowtorzen)
+    przejscie2 <- rbind(przejscie2,
+                       data.table(nOfBuyers = i,
+                                  nOfSellers = j,
+                                  efficiency = runSimulation(report = "efficiency")))
+    print(paste(i, j))
+  }
+}
+przejscie2agg <- przejscie2[,
+                            .(efficiency = median(efficiency.efficiency)),
+                            by = .(nOfBuyers, nOfSellers)]
+
+# efektywnosc a liczba uczestnikow
+lUczestnikow <- data.table()
+for (i in seq(8, 180, by = 4))
+{
+  parametrize(nBuyers = i / 2,
+              nSellers = i / 2)
+  for (a in 1:ilePowtorzen)
+  {
+    lUczestnikow <- rbind(lUczestnikow,
+                          data.table(liczbaUczestnikow = i,
+                                     runSimulation(report = "efficiency")))
+  }
+  print(Sys.time())
+  print(i)
+}
+lUczestnikowAgg <- lUczestnikow[,
+                                .(efficiency = median(efficiency)),
+                                by = .(liczbaUczestnikow)]
+
+save.image("2.RData")
+min(lUczestnikowAgg$efficiency)
+which.min(lUczestnikowAgg$efficiency)
 
 
+# dokladniej
+doklUcz <- c()
+parametrize(nBuyers = lUczestnikowAgg[which.min(lUczestnikowAgg$efficiency)]$liczbaUczestnikow / 2,
+            nSellers = lUczestnikowAgg[which.min(lUczestnikowAgg$efficiency)]$liczbaUczestnikow / 2)
+for (a in 1:ilePowtorzen * 4)
+{
+  doklUcz <- append(doklUcz,
+                    runSimulation(report = "efficiency"))
+  print(a)
+}
+dokladn <- data.table(dummy = lUczestnikowAgg[which.min(lUczestnikowAgg$efficiency)]$liczbaUczestnikow, efficiency = as.numeric(doklUcz))
 
-# data generation ---------------------------------------------------------
+ggplot(dokladn, aes(x = dummy, y = efficiency)) +
+  geom_boxplot() + geom_point(size = 4, shape = 4) + theme_bw() +
+  ggtitle(paste0("Rozklad realizacji zmiennej dla ",
+                 lUczestnikowAgg[which.min(lUczestnikowAgg$efficiency)]$liczbaUczestnikow,
+                 " uczestników rynku")) +
+  scale_x_continuous(name = "", breaks = c())
 
-df <- data.frame(NULL)
-temp <- data.frame(NULL)
-nBuyersV = c(); nSellersV = c(); maxBValV = c(); maxSCostV = c()
+czyPrzypadek <- ifelse(min(lUczestnikowAgg$efficiency) < quantile(dokladn$efficiency, .25),
+                       yes = "Jak widać, niski wynik był ekstremalną, mało prawdopodobną realizacją zmiennej losowej.",
+                       no = "Jak widać, niski wynik nie był jedynie ekstremalną realizacją zmiennej losowej. Można podejrzewać, że  w wyniku interakcji nieoptymalnej liczby agentów dochodzi tutaj do obniżenia efektywności zastosowanego mechanizmu rynkowego.")
+czyPrzypadek <- unname(czyPrzypadek)
 
+setwd(path)
+NLQuit()
+setwd(oldPath)
 
-# NLDoCommand(1, "setup")
-# NLDoWhile(condition = "(efficiency < 90) and (ticks < maxNumberOfTrades)",
-#           command = "go")
-df <- data.frame()
-# parametrizeAndRun()
-df <- rbind(df, parametrizeAndRun())
-print(df)
-df <- rbind(df, parametrizeAndRun())
-print(df)
-df <- rbind(df, parametrizeAndRun())
-print(df)
+render(input = "KCiulkin.Rmd")
 
-temp <- data.frame(NLReport(reporter = c("ticks", "efficiency", "transactionPrice", "actualSurplus")))
-colnames(df) <- c()
-
-
-
-ggplot(df) +
-  geom_line(aes(x = tick,
-                 y = eff)) +
-  coord_cartesian(ylim = c(0, 100)) +
-  theme_bw()
-
-NLQuit(all = T)
 
 
